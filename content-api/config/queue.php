f1bf94fdaac881e85c2093d1da69cd14ee451cb4
benjamim <bhaskdelta2@gmail.com>
2025-05-04 18:45:06 -0300

    finish setting rabbitmq

--- content-api/config/queue.php
+++ content-api/config/queue.php
@@ -29,6 +29,18 @@ return [
     */
 
     'connections' => [
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', 'rabbitmq'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+        ],
 
         'sync' => [
             'driver' => 'sync',
