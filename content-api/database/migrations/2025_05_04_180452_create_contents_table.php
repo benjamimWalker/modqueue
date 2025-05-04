401b8d89c69e84d0b7d3faae35af5855e3682032
benjamim <bhaskdelta2@gmail.com>
2025-05-04 16:12:44 -0300

    make content's title nullable

--- content-api/database/migrations/2025_05_04_180452_create_contents_table.php
+++ content-api/database/migrations/2025_05_04_180452_create_contents_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('contents', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->string('body');
             $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
             $table->timestamps();
